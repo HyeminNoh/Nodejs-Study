aeced07b7d0a224f683a8e59bfc717511b3780f9
hyeminnoh <shrmffkfk@naver.com>
2020-03-14 03:14:50 +0900

    동작안해서 수정

--- CreateHttpServer/server3.js
+++ CreateHttpServer/server3.js
@@ -1,20 +1,20 @@
 const http = require('http');
-
+// CREDIT: 유인동님이 알려주신 함수
 const parseCookies = (cookie = '') =>
-    cookie
+  cookie
     .split(';')
-    .map(v=>v.split('='))
-    .map(([k, ...vs])=>[k, vs.join('=')])
-    .reduce((acc, [k, v])=>{
-        acc[k.trim()]=decondeURIComponent(v);
-        return acc;
-    },{});
+    .map(v => v.split('='))
+    .reduce((acc, [k, v]) => {
+      acc[k.trim()] = decodeURIComponent(v);
+      return acc;
+    }, {});
 
 http.createServer((req, res) => {
-    const cookies = parseCookies(req.headers.cookie);
-    console.log(req.url, cookies);
-    res.writeHead(200, {'Set-cookie': 'mycookie=test'});
-    res.end('Hello Cookie');
-}).listen(8082, ()=>{
-    console.log('8082번 포트에서 서버 대기 중입니다!');
-});
+  const cookies = parseCookies(req.headers.cookie);
+  console.log(req.url, cookies);
+  res.writeHead(200, { 'Set-Cookie': 'mycookie=test' });
+  res.end('Hello Cookie');
+})
+  .listen(8082, () => {
+    console.log('8082번 포트에서 서버 대기중입니다!');
+  });
