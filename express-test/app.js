0d4aaa7e7008e1901c505995fb79a2d41e54b619
hyeminnoh <shrmffkfk@naver.com>
2020-03-15 19:05:26 +0900

    express 서버 이해 이해한 내용 주석 추가

--- express-test/app.js
+++ express-test/app.js
@@ -7,17 +7,26 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// express 패키지를 호출하여 app 변수 객체 생성
 var app = express();
 
-// view engine setup
+// 익스프레스 앱을 설정
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// 커스텀 미들웨어 만들어보기
+app.use(function(req,res, next){
+  console.log(req.url, '커스텀 미들웨어!');
+  // 반드시 next를 호출해야 다음으로 넘어감
+  //next();
+});
+
+// 미들웨어 연결
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -38,4 +47,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// app 객체를 모듈로 만들어 줌, 이것이 bin/www에서 사용된 app 모듈
 module.exports = app;
