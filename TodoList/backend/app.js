8ffd6f4ac63f65c3a1e5f66441d06ea790dc9a77
hyeminnoh <shrmffkfk@naver.com>
2020-03-20 16:34:13 +0900

    오타 수정

--- TodoList/backend/app.js
+++ TodoList/backend/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var taskRouter = require('./routes/task');
+var taskRouter = require('./routes/tasks');
 var connect = require('./schemas');
 
 var app = express();
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/task', taskRouter);
+app.use('/tasks', taskRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
