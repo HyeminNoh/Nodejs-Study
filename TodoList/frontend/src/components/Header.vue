db9a9de3a850d3fbcaf07fc889b91bd1573e5954
hyeminnoh <shrmffkfk@naver.com>
2020-03-21 13:09:23 +0900

    Header

---
+++ TodoList/frontend/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <dir id="Header">
+    <nav>
+      <router-link to="/" class="items">오늘 할 일</router-link>
+    </nav>
+  </dir>
+</template>
+
+<style lang="css">
+#Header nav {
+  width: 95%;
+  color: black;
+  font-weight: bold;
+  min-height: 50px;
+  font-family: comfortaa;
+}
+#Header .items {
+  margin-top: 1.4em;
+  float: left;
+  margin-left: 30px;
+  outline: none;
+  font-size: 20px;
+  color: black;
+  text-decoration: none;
+}
+#Header .items:hover {
+  outline: none;
+  color: #ff4d5a;
+  text-decoration: none;
+}
+</style>
