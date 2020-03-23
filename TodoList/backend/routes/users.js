d5e52cc4309b95999a4e8f01765d38113622dcef
hyeminnoh <shrmffkfk@naver.com>
2020-03-24 00:57:21 +0900

    삭제, 추가 요청 추가

--- TodoList/backend/routes/users.js
+++ TodoList/backend/routes/users.js
@@ -1,9 +1,64 @@
 var express = require('express');
+const moment = require('moment');
+var User = require('../schemas/user');
+
 var router = express.Router();
+
+moment.tz.setDefault("Asia/Seoul");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find({})
+  .then((user)=>{
+    console.log(user);
+    res.json(user);
+  })
+  .catch((err)=>{
+    console.error(err);
+    next(err);
+  })
+});
+
+router.get('/:name', function(req, res, next){
+  User.find({name: req.params.name})
+  .then((user)=>{
+      console.log(user);
+      res.json(user);
+  })
+  .catch((err)=>{
+      console.error(err);
+      next(err);
+  });
+});
+
+router.post('/', function(req, res, next){
+  const user = new User({
+    name: req.body.name,
+    pwd: req.body.title,
+    createdAt: moment().format('YYYY-MM-DD')
+  });
+  user.save()
+  .then((result)=>{
+      return User.populate(result, {path: 'userId'});
+  })
+  .then((result)=>{
+      res.status(201).json(result);
+  })
+  .catch((err)=>{
+      console.error(err);
+      next(err);
+  });
+});
+
+router.delete('/:name', function(req, res, next){
+  Task.remove({name: req.params.name})
+  .then((result)=>{
+      res.json(result);
+  })
+  .catch((err)=>{
+      console.error(err);
+      next(err);
+  });
 });
 
 module.exports = router;
