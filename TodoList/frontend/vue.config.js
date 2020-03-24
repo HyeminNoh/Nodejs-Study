720d172a190795771a7e3ad7cc54a9b4b6cff41b
hyeminnoh <shrmffkfk@naver.com>
2020-03-25 01:41:50 +0900

    cors proxy server 설정

--- TodoList/frontend/vue.config.js
+++ TodoList/frontend/vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/': {
-                "target": 'http://localhost:8080/',
+                "target": 'http://localhost:3000/',
                 "pathRewrite": { '^/': '' },
                 "changeOrigin": true,
                 "secure": false
