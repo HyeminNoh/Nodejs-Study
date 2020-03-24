e04ba7488278b93568dda53d3ad2ecc0f03ace5d
hyeminnoh <shrmffkfk@naver.com>
2020-03-24 18:52:08 +0900

    스키마 수정

--- TodoList/backend/schemas/user.js
+++ TodoList/backend/schemas/user.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const moment = require('moment');
-moment.tz.setDefault("Asia/Seoul");
 
 const { Schema } = mongoose;
 const userSchema = new Schema({
@@ -12,10 +10,9 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
-    createdAt: {
-        type: String,
-        default: moment().format('YYYY-MM-DD'),
-    },
+    jsonWebToken: {
+        type: String
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
