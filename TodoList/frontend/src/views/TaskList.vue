6ebc1c7c0070351f1a68d415ddc762535bfad6e0
hyeminnoh <shrmffkfk@naver.com>
2020-03-25 01:43:21 +0900

    토큰 체크, 로그인되어있는 사용자 정보 요청

--- TodoList/frontend/src/views/TaskList.vue
+++ TodoList/frontend/src/views/TaskList.vue
@@ -1,41 +1,52 @@
 <template>
   <div class="tasklist">
     <div id="head">
-        <h2>{{name}}해야될 일이 요만큼</h2>
+      <h2>{{name}} 해야될 일이 요만큼</h2>
     </div>
-    <b-table>
-      
-    </b-table>
+    <b-table></b-table>
   </div>
 </template>
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   data() {
-    return{   
+    return {
       name: "",
-    }
+      userData: [],
+    };
   },
-  created:{
-    userCheck: function(){
-      var token = sessionStorage.getItem('token');
-      axios.get('http://localhost:3000/users/me',{ token: token })
-      .then((result)=>{
-        if(result.type==true){
-          this.name = result.name
-        }
-        else{
-          return this.$route.go(-1)
-        }
-      })
-      return this.$route.params;
+  created() {
+    this.userCheck()
+  },
+  methods: {
+    userCheck: function() {
+      var token = sessionStorage.getItem("token");
+      axios
+        .get("/users/me", {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Authorization': 'Bearer ' + token
+          }
+        })
+        .then(result => {
+          if (result.data.type == true) {
+            this.name = result.data.data.name;
+            this.userData = result.data.data;
+            this.loadData();
+          } else {
+            return this.$route.go(-1);
+          }
+        });
+    },
+    loadData: function(){
+      
     }
   }
-}
+};
 </script>
 <style>
-#head{
+#head {
   text-align: center;
   background-color: gainsboro;
   margin: 5%;
