4a9e9e0331a9e28f7a1d18b1698f4b25f92d6532
hyeminnoh <shrmffkfk@naver.com>
2020-03-25 23:57:16 +0900

    리스트 출력, 등록 기능

--- TodoList/frontend/src/views/TaskList.vue
+++ TodoList/frontend/src/views/TaskList.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="tasklist">
-    <div id="head">
-      <h2>{{name}} 해야될 일이 요만큼</h2>
+    <b-input-group :prepend="this.name">
+      <b-form-input id="task-input" v-model="taskInput" required></b-form-input>
+      <b-input-group-append>
+        <b-button @click="submit" variant="outline-success">추 가</b-button>
+      </b-input-group-append>
+    </b-input-group>
+    <div id="task-lists">
+      <b-list-group v-if="taskState && taskList.length">
+        <b-list-group-item class="d-flex justify-content-between" v-for="(item, index) in taskList" :key="index">
+          <b-form-checkbox size="lg">
+            <p>{{item.title}}</p>
+          </b-form-checkbox>
+          <div>
+            <b-button style="margin-right:10px;">수 정</b-button>
+            <b-button >삭 제</b-button>
+          </div>
+        </b-list-group-item>
+      </b-list-group>
     </div>
-    <b-table></b-table>
   </div>
 </template>
 <script>
@@ -14,10 +29,13 @@ export default {
     return {
       name: "",
       userData: [],
+      taskState: false,
+      taskList: [],
+      taskInput: "",
     };
   },
   created() {
-    this.userCheck()
+    this.userCheck();
   },
   methods: {
     userCheck: function() {
@@ -25,8 +43,8 @@ export default {
       axios
         .get("/users/me", {
           headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Authorization': 'Bearer ' + token
+            "Access-Control-Allow-Origin": "*",
+            Authorization: "Bearer " + token
           }
         })
         .then(result => {
@@ -39,17 +57,57 @@ export default {
           }
         });
     },
-    loadData: function(){
-      
+    loadData: function() {
+      axios
+        .get("/tasks/" + this.userData._id, {
+          headers: {
+            "Access-Control-Allow-Origin": "*"
+          }
+        })
+        .then(result => {
+          if (result.data.type == true) {
+            this.taskState = "true";
+            this.taskList = result.data.data;
+          } else {
+            this.taskState = "false";
+            var cards = document.getElementById("task-cards");
+            var notask = document.createElement("div");
+            notask.innerText = this.name + " " + result.data.data + " 😝";
+            notask.id = "notask";
+            cards.appendChild(notask);
+          }
+        });
+    },
+    submit: function() {
+      axios
+        .post("/tasks/", {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-type": "application/x-www-form-urlencoded"
+          },
+          id: this.userData._id,
+          title: this.taskInput,
+          completed: false
+        })
+        .then(result => {
+          console.log(result);
+          this.$route.push("/");
+        });
     }
   }
 };
 </script>
 <style>
-#head {
-  text-align: center;
-  background-color: gainsboro;
+.tasklist {
   margin: 5%;
-  padding: 5%;
+}
+#task-lists {
+  margin-top: 20px;
+}
+#notask {
+  margin-top: 5%;
+  text-align: center;
+  font-weight: bold;
+  font-size: 30px;
 }
 </style>
