2ee2d6f23ac8cd9034df762649cf5bcc39d51c7d
hyeminnoh <shrmffkfk@naver.com>
2020-03-31 00:00:22 +0900

    learn-mocha project 생성

---
+++ learn-mocha/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+
+var users = [
+    {id: 1, name: 'tufu'},
+    {id: 2, name: 'love'},
+    {id: 3, name: 'hyemin'},
+];
+app.use(morgan('dev'));
+
+app.get('/users', function(req, res){
+    res.json(users);
+});
+
+app.listen(3000, function(){
+    console.log('app listening on port 3000!');
+});
