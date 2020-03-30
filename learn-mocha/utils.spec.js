96296bbaf3229b6a2a3df9de96acbe7a95bc9fea
hyeminnoh <shrmffkfk@naver.com>
2020-03-31 00:24:17 +0900

    should 적용

--- learn-mocha/utils.spec.js
+++ learn-mocha/utils.spec.js
@@ -1,10 +1,10 @@
 const utils = require('./utils');
-const assert = require('assert');
+const should = require('should');
 
 describe('utils.js모듈의 capitialize() 함수는', ()=>{
     it('문자열의 첫번째 문자를 대문자로 반환한다.', ()=>{
         // test code...
         const result = utils.capitialize('hello');
-        assert.equal(result, 'Hello');
+        result.should.be.equal('Hello');
     });
 });
