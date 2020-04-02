1ebf67912dbc5e9e3c53dbf7e41416cde843a7d7
hyeminnoh <shrmffkfk@naver.com>
2020-04-03 02:13:31 +0900

    세션 체크로 로그인 유무 확인

--- learn-oauth/routes/index.js
+++ learn-oauth/routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const authenticateUser = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(301).redirect('/auth');
+  }
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', authenticateUser, function(req, res, next) {
   res.render('index', { title: 'OAuth2.0 공부한다 👩‍💻' });
 });
 
