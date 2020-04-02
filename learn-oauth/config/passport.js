1c4b385260049adaf2f6bd6a6ae067496dbd765d
hyeminnoh <shrmffkfk@naver.com>
2020-04-03 02:12:14 +0900

    인증 후 return 수정

--- learn-oauth/config/passport.js
+++ learn-oauth/config/passport.js
@@ -14,12 +14,8 @@ module.exports = (passport) => {
             clientID: "a51d7f734aeb8afa214b",
             clientSecret: "266a4c83efd184917d583adaa24facda48c06c67",
             callbackURL: 'http://127.0.0.1:3000/auth/github/callback'
-        }, function (accessToken, refreshToken, profile, done) {
-            const socialId = profile.id;
-            const nickname = profile.username;
-            const profileImageUrl = profile.photos[0].value;
-
-            onLoginSuccess('Github', socialId, nickname, profileImageUrl, done);
+        }, function (accessToken, refreshToken, profile, cb) {
+            return cb(null, profile);
         }
     ));
 
@@ -28,29 +24,8 @@ module.exports = (passport) => {
             clientSecret: "ZO3RUwgAPL0jw5YEer7drGwo",
             callbackURL: 'http://localhost:3000/auth/google/callback',
             scope: ['https://www.googleapis.com/auth/plus.me']
-        }, function (accessToken, refreshToken, profile, done) {
-            const socialId = profile.id;
-            const nickname = profile.displayName;
-            const profileImageUrl = profile.photos[0].value;
-
-            onLoginSuccess('Google', socialId, nickname, profileImageUrl, done);
+        }, function (accessToken, refreshToken, profile, cb) {
+            return cb(null, profile);    
         }
     ));
-
-    function onLoginSuccess(platformName, socialId, nickname, profileImageUrl, done) {
-        userService.findOrCreate(platformName, socialId, nickname, profileImageUrl)
-            .spread((user, created) => {
-                if (user.state === 1) {
-                    userService.updateUserToJoinedState(user)
-                        .then(result => {
-                            done(null, user);
-                        })
-                        .catch(err => {
-                            done(null, user);
-                        })
-                } else {
-                    done(null, user);
-                }
-            });
-    }
 };
