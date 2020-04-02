9e0e7672bb52d3abbc8ca204fdd4285a80e02cac
hyeminnoh <shrmffkfk@naver.com>
2020-04-03 02:12:25 +0900

    버튼 이벤트

---
+++ learn-oauth/public/javascripts/index.js
@@ -0,0 +1,3 @@
+document.getElementById('logout-btn').addEventListener('click', function (e) {
+    window.location.href = '/auth/logout';
+});
---
+++ learn-oauth/public/javascripts/login.js
@@ -0,0 +1,6 @@
+document.getElementById('google-btn').addEventListener('click', function (e) {
+    window.location.href = '/auth/google';
+});
+document.getElementById('github-btn').addEventListener('click', function (e) {
+    window.location.href = '/auth/github';
+});
