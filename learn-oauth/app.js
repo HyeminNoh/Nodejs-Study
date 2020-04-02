768f8044ee677f3d5dacf4e6ffb6ac3f70f89fe2
hyeminnoh <shrmffkfk@naver.com>
2020-04-02 22:50:37 +0900

    passport, session 설정

--- learn-oauth/app.js
+++ learn-oauth/app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+
+var SECRET_CODE = 'secret';
 
 var app = express();
 
@@ -15,12 +19,23 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: SECRET_CODE,
+  cookie: { maxAge: 60 * 60 * 1000 },
+  resave: true,
+  saveUninitialized: false
+}));
+
+require('./config/passport')(passport);
+app.use(passport.initialize());
+app.use(passport.session()); //로그인 세션 유지
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
